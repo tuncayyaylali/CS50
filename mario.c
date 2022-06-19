3542b6b4cb307baba436f0c235db57e2dab83cb7
Tuncay YAYLALI <81981206+tuncayyaylali@users.noreply.github.com>
2022-06-19 13:07:58 +0300

    Create mario.c
    Homework for Week 1 in CS50

---
+++ mario.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_positive_int(string propmt);
+
+int main(void)
+{
+    int i = get_positive_int("Positive integer: ");
+    for (int j=1; j<i+1; j++)
+    {
+        for (int k=0; k<i-j; k++)
+        {
+            printf(" ");
+        }
+        for (int l=0; l<j; l++)
+        {
+            printf("#");
+        }
+        printf(" ");
+        for (int l=0; l<j; l++)
+        {
+            printf("#");
+        }      
+
+        printf("\n");
+    }
+}
+
+int get_positive_int(string prompt)
+{
+    int number;
+    do
+    {
+        number = get_int("%s", prompt);       
+    }
+    while (number<1 || number>8);
+    return number;
+}
